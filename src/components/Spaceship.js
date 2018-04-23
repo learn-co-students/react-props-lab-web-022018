22f484a4972d1e8ad64675d76be1d2ea6d860fcd
alec-petros <a.petros@verizon.net>
2018-04-23 14:53:53 -0400

    Done.

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -7,9 +7,9 @@ class Spaceship extends React.Component {
     return (
       <div>
         <h1> {this.props.name} </h1>
-        <h3>Speed: {this.props.speed}</h3>
-        <p>Has Rockets: {this.props.hasRockets}</p>
-        <p>Colors: {this.props.colors}</p>
+        <h3>Speed: {!this.props.speed ? "Slow" : this.props.speed}</h3>
+        <p>Has Rockets: {this.props.hasRockets ? "Yes" : "No"}</p>
+        <p>Colors: {!this.props.colors? ['black', 'red'] : this.props.colors}</p>
       </div>
     )
   }
